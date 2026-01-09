c90bf367d2b57413c5ed2d4032baae8731846b72
AlwynRajivS <119982824+AlwynRajivS@users.noreply.github.com>
2026-01-09 12:05:55 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -4,6 +4,6 @@
  * Paste your Google Apps Script Web App URL here.
  * If empty, the app will run in MOCK MODE for demonstration.
  */
-export const GAS_URL = ""; 
+export const GAS_URL = "https://script.google.com/macros/s/AKfycbzxEQegsB4aA94bYw8WwGiX9zV0MxnLHCYnWa6MOXSPdjGNkWJPOUhmqSgJ3_JFDtTO/exec"; 
 
 export const API_ENABLED = GAS_URL !== "";
